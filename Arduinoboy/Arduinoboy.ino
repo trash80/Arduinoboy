fb34314d42b17e545460761c2f4ebd2ee2b0c694
timothy lamb <trash80@gmail.com>
2015-07-26 15:01:53 -0700

    Release 0.3.1 from Google Code.

--- Arduinoboy/Arduinoboy.ino
+++ Arduinoboy/Arduinoboy.ino
@@ -1,8 +1,8 @@
 /**************************************************************************
  *         A     R     D     U     I     N     O     B     O     Y        *
  *                                                                        *
- * Version: 0.2.1                                                         *
- * Date:    Feb 24 2008                                                   *
+ * Version: 0.3.1                                                         *
+ * Date:    Feb 25 2008                                                   *
  * Name:    Timothy Lamb                                                  *
  * Email:   trash80@gmail.com                                             *
  *                                                                        *
@@ -16,84 +16,206 @@
  *                                                                         *
  ***************************************************************************/
  
-int syncOut = 12;    // clock out to gameboy
-int dataOut = 11;    // serial data to gameboy
-int midiEnable = 10; // power opticouplah
+ 
+/***************************************************************************
+* Simple User Settings
+***************************************************************************/
 
-int statusLed = 8; // power light
-int clockLed = 9;  // start light
+int syncEffectsMidiChannel = 8; //midi sync effects, not implemented yet
+int mode = 0;  //Mode 0: Midi Input to LSDJ Sync, Mode 1: LSDJ MASTER to Midi output
 
-int sigCount = 0;   //for loop int (we need to cycle 8 pulses)
+/***************************************************************************
+* Lets Assign our Arduino Pins .....
+***************************************************************************/
+int gbClockOut = 12;    // clock out to gameboy
+int gbSerialOut = 11;    // serial data to gameboy
 
-boolean seqStart = false;    //Sequencer Started
-byte incomingByte;  //incomming midi message
+int gbClockIn = 2;    // clock in from gameboy
+int gbSerialIn = 3;    // serial data from gameboy
+
+int midiInputPower = 10; // power pin for midi input opto-isolator
+
+int ledSequencerStop = 8; // stopped light pin
+int ledSequencerStart = 9;  // start light pin
+
+/***************************************************************************
+* Switches and states
+***************************************************************************/
+boolean sequencerStarted = false;        //Sequencer has Started
+boolean midiSyncEffectsHalfTime = false; //Not Implemented yet
+
+/***************************************************************************
+* Counter vars
+***************************************************************************/
+int countLSDJTicks = 0;            //for loop int (we need to cycle 8 pulses)
+boolean countSyncHalfTime = true;  //future feature, Not implemented
+int countGbClockTicks =0;
+int countClockPause =0;
+int countIncommingMidiByte =0;
+
+
+/***************************************************************************
+* Inbound Data Placeholders
+***************************************************************************/
+byte incomingMidiByte;  //incomming midi message
+byte readGbClockIn;
+byte readGbSerialIn;
+
+int incomingMidiNote = 0;
+int incomingMidiVel = 0;
+
 
 void setup() {
-  // Setup our ports as outputs
-  pinMode(statusLed,OUTPUT); 
-  pinMode(clockLed,OUTPUT); 
-  pinMode(syncOut,OUTPUT); 
-  pinMode(dataOut,OUTPUT); 
-  pinMode(midiEnable,OUTPUT); 
+/*
+  Init Output Pins
+*/
+  pinMode(ledSequencerStop,OUTPUT); 
+  pinMode(ledSequencerStart,OUTPUT); 
+  pinMode(gbClockOut,OUTPUT); 
+  pinMode(gbSerialOut,OUTPUT); 
+  pinMode(midiInputPower,OUTPUT); 
   
-  Serial.begin(31250);	  //midi baudrate
+/*
+  Init Input Pins
+*/
+  pinMode(gbClockIn,INPUT); 
+  pinMode(gbSerialIn,INPUT);
   
-  digitalWrite(midiEnable, HIGH); // turn on the optoisolator
-  digitalWrite(syncOut, HIGH);    // gameboy wants a HIGH line
-  digitalWrite(dataOut, LOW);    // no data to send
+/*
+  Set MIDI Serial Rate
+*/
+  Serial.begin(31250);
   
-  blinkStatus(5,80);              // blink our status led 5 times
-  digitalWrite(statusLed, HIGH);  // then turn it on
+/*
+  Set Pin States
+*/
+  digitalWrite(midiInputPower, HIGH); // turn on the optoisolator
+  digitalWrite(gbClockOut, HIGH);    // gameboy wants a HIGH line
+  digitalWrite(gbSerialOut, LOW);    // no data to send
+  digitalWrite(ledSequencerStop, HIGH);  // then turn it on
+  
+/*
+  Misc Startup
+*/
+  syncEffectsMidiChannel = 143 + syncEffectsMidiChannel; //set the midi channel to the real number (144 to 159)
 }
 
 void loop () {
-  if (Serial.available() > 0) {
-    incomingByte = Serial.read();
-    switch (incomingByte) {
-    case 248: // Clock Message Recieved
-      //send a clock tick out if the sequencer is runn
-      if(seqStart == true) {
-        sendClockTick();
+  switch(mode) {
+    case 0:
+     modeGameboySlaveSync();
+     break;
+    case 1:
+     modeGameboyMasterSync();
+     break;
+  }
+}
+
+void modeGameboyMasterSync()
+{
+  while(1){
+  readGbClockIn = digitalRead(gbClockIn); //Read gameboy's clock line
+  if(readGbClockIn) {
+    while(readGbClockIn) {
+      countClockPause++;
+      if(sequencerStarted && countClockPause > 8000) {
+          Serial.print(0xFC, BYTE);
+          countClockPause = 0;
+          sequencerStop();
       }
-      break;
-    case 250: // Transport Start Message
-    case 251: // Transport Continue Message
-      sequencerStart();
-      break;
-    case 252: // Transport Stop Message
-      sequencerStop();
-      break;
+      readGbClockIn = digitalRead(gbClockIn);
     }
+    countClockPause = 0;
+    countGbClockTicks++;
+    if(countGbClockTicks == 8) {
+      countGbClockTicks=0;
+      if(!sequencerStarted) {
+          Serial.print(0xFA, BYTE);
+          sequencerStart();
+      }
+      Serial.print(0xF8, BYTE);
+    }
+  }
+  }
+}
+
+void modeGameboySlaveSync()
+{
+  while(1){
+  if (Serial.available() > 0) {
+    incomingMidiByte = Serial.read();
+    switch (incomingMidiByte) {
+      case 248: // Clock Message Recieved
+        //send a clock tick out if the sequencer is runn
+        if(sequencerStarted && midiSyncEffectsHalfTime && countSyncHalfTime
+          || sequencerStarted && !midiSyncEffectsHalfTime) {
+          sendClockTickToLSDJ();
+        }
+        if(midiSyncEffectsHalfTime) countSyncHalfTime = !countSyncHalfTime;
+        break;
+      case 250: // Transport Start Message
+      case 251: // Transport Continue Message
+        sequencerStart();
+        break;
+      case 252: // Transport Stop Message
+        sequencerStop();
+        break;
+      default:
+        if(incomingMidiByte == syncEffectsMidiChannel) {
+           // This doesnt work yet
+           // getNote();
+        }
+        break;
+    }
+  } 
   }
 }
 
 void sequencerStart()
 {
-  digitalWrite(clockLed, HIGH);
-  digitalWrite(statusLed, LOW);
-  seqStart = true;
+  digitalWrite(ledSequencerStart, HIGH);
+  digitalWrite(ledSequencerStop, LOW);
+  sequencerStarted = true;
 }
 
 void sequencerStop()
 {
-  digitalWrite(clockLed, LOW);
-  digitalWrite(statusLed, HIGH);
-  seqStart = false;
+  digitalWrite(ledSequencerStart, LOW);
+  digitalWrite(ledSequencerStop, HIGH);
+  midiSyncEffectsHalfTime = false;
+  sequencerStarted = false;
 }
 
-void sendClockTick()
+void sendClockTickToLSDJ()
 {
-  for(sigCount=0;sigCount<8;sigCount++) {
-    digitalWrite(syncOut,LOW);digitalWrite(syncOut,HIGH);
+  for(countLSDJTicks=0;countLSDJTicks<8;countLSDJTicks++) {
+    digitalWrite(gbClockOut,LOW);digitalWrite(gbClockOut,HIGH);
   }
 }
 
-void blinkStatus(int times, int period)
+void getNote()
 {
-  for(int count=0; count < times; count++) {
-      digitalWrite(statusLed,HIGH);
-      delay(period);
-      digitalWrite(statusLed,LOW);
-      delay(period);
+  countIncommingMidiByte = 0;
+  while(countIncommingMidiByte < 2) {
+    if (Serial.available() > 0) {
+      countIncommingMidiByte++;
+      incomingMidiByte = Serial.read();
+      if(countIncommingMidiByte == 1) {
+        incomingMidiNote = incomingMidiByte;
+      } else if(incomingMidiByte > 0) {
+        switch(incomingMidiNote) {
+          case 60:
+            sequencerStart();
+            break;
+          case 61:
+            sequencerStop();
+            break;
+          case 62:
+            midiSyncEffectsHalfTime = !midiSyncEffectsHalfTime;
+            countSyncHalfTime = true;
+            break;
+         }
+      }
+    }
   }
 }
